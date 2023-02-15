449a40b9c2419cf79bfc1787c3fbb1207ebc18fa
android-cat <58460713android-cat@users.noreply.github.com>
2023-02-15 09:35:05 +0900

    typescript対応

--- src/resources/js/app.tsx
+++ src/resources/js/app.tsx
@@ -4,9 +4,9 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 
 const App = () => {
-    const title = "Laravel 9 Mix with TypeScript React !!";
+    const title: string = "Laravel 9 Mix with TypeScript React !!";
     return <h1>{title}</h1>;
 };
-const container = document.getElementById('app');
+const container = document.getElementById('app') as HTMLBaseElement;
 const root = createRoot(container);
 root.render(<App />);

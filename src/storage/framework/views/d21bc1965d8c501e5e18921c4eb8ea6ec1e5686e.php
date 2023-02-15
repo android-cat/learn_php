2aae35a927effc9d76e82a54667d7e3507869e87
android-cat <58460713android-cat@users.noreply.github.com>
2023-02-15 09:37:02 +0900

    react+typescript+laravel環境構築完了

---
+++ src/storage/framework/views/d21bc1965d8c501e5e18921c4eb8ea6ec1e5686e.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="<?php echo e(mix('/js/app.js')); ?>"></script>
+    </body>
+</html><?php /**PATH /var/www/html/resources/views/welcome.blade.php ENDPATH**/ ?>

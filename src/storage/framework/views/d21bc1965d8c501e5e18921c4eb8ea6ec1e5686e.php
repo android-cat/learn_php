0d0cba265582c1d4ae474826360b0ce26ebd4cba
android-cat <58460713android-cat@users.noreply.github.com>
2023-02-22 16:44:37 +0900

    CSSの修正漏れ修正

--- src/storage/framework/views/d21bc1965d8c501e5e18921c4eb8ea6ec1e5686e.php
+++ src/storage/framework/views/d21bc1965d8c501e5e18921c4eb8ea6ec1e5686e.php
@@ -3,10 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <link rel="stylesheet" href="<?php echo e(mix('/css/app.css')); ?>">
+        <title>積みゲー管理</title>
     </head>
     <body>
-        <div id="app"></div>
-        <script src="<?php echo e(mix('/js/app.js')); ?>"></script>
+        <div id="root"></div>
+        <script src="<?php echo e(mix('/js/index.js')); ?>"></script>
     </body>
 </html><?php /**PATH /var/www/html/resources/views/welcome.blade.php ENDPATH**/ ?>

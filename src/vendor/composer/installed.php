d472a4c8923bd89af0c93c724960f54a2a92b97d
android-cat <58460713android-cat@users.noreply.github.com>
2023-02-24 14:46:52 +0900

    laravel/breeze導入

--- src/vendor/composer/installed.php
+++ src/vendor/composer/installed.php
@@ -364,6 +364,15 @@
                 0 => '*',
             ),
         ),
+        'laravel/breeze' => array(
+            'pretty_version' => 'v1.19.2',
+            'version' => '1.19.2.0',
+            'reference' => '725e0c4fb1f630afdd90b5fba2a7f6d8d547ac29',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laravel/breeze',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'laravel/framework' => array(
             'pretty_version' => 'v9.51.0',
             'version' => '9.51.0.0',
